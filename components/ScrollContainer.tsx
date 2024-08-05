2969c274bc21eba9f6361f5868dc472ad9a7d445
Rachit Kumar <rachit.prodigy205@gmail.com>
2024-08-06 00:15:00 +0530

    fix: finish navigation app with search

--- components/ScrollContainer.tsx
+++ components/ScrollContainer.tsx
@@ -5,6 +5,17 @@ import { Input } from "@/components/ui/input";
 import NavigationCard from "@/components/NavigationCard";
 import { Places } from "@/models/interfaces";
 import Map from "@/components/Map";
+import {
+    Drawer, DrawerClose,
+    DrawerContent,
+    DrawerDescription, DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
+import { ChevronUp } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 interface ScrollContainerProps {
     places: Places[];
@@ -13,6 +24,8 @@ interface ScrollContainerProps {
 const ScrollContainer: React.FC<ScrollContainerProps> = ({ places }) => {
     const [showHeader, setShowHeader] = useState(true);
     const [lastScrollY, setLastScrollY] = useState(0);
+    const [bottomDrawerStatus, setBottomDrawerStatus] = useState<boolean>(false);
+    const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(() => {
         const handleScroll = () => {
@@ -32,9 +45,16 @@ const ScrollContainer: React.FC<ScrollContainerProps> = ({ places }) => {
         };
     }, [lastScrollY]);
 
+    const uniquePlaces = Array.from(new Set(places.map(place => place.name)))
+        .map(name => places.find(place => place.name === name));
+
+    const filteredPlaces = uniquePlaces.filter(place =>
+        place?.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
     return (
-        <div className={'h-screen w-full bg-white flex flex-row'}>
-            <div className={'h-full w-1/3 bg-snuxplore-brown flex flex-col px-8 pt-8 justify-start items-center'}>
+        <div className={'h-screen w-full bg-white flex flex-col md:flex-row overflow-y-hidden md:overflow-y-scroll'}>
+            <div className={'h-full hidden w-1/3 bg-snuxplore-brown md:flex flex-col px-8 pt-8 justify-start items-center'}>
                 <div className={`w-full h-fit text-3xl text-white flex flex-row items-center transition-transform duration-300 font-nohemi-semibold ${showHeader ? 'translate-y-0' : '-translate-y-full'}`}>
                     <img src={"/s.svg"} className={'mr-2'} />SNUXplore
                 </div>
@@ -43,17 +63,53 @@ const ScrollContainer: React.FC<ScrollContainerProps> = ({ places }) => {
                     {"We get it, navigating the university can be challenging! But have no worries, We can connect you to resources that will unlock all that Shiv Nadar University has to offer."}
                 </div>
                 <div className={`mt-8 w-full transition-transform duration-300 font-nohemi-regular ${showHeader ? 'translate-y-0' : '-translate-y-full'}`}>
-                    <Input className={'w-full'} placeholder={"Search for location, admin info or anything else"} />
+                    <Input
+                        className={'w-full'}
+                        placeholder={"Search for location, admin info or anything else"}
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
                 </div>
                 <div className={'w-full mt-8 h-full overflow-y-scroll space-y-4 no-scrollbar'}>
-                    {places.map((place) => (
-                        <NavigationCard key={place.name} place={place} />
+                    {filteredPlaces.map((place) => (
+                        <NavigationCard key={place?.name} place={place as Places} />
                     ))}
                 </div>
             </div>
 
-            <div className={'h-full w-2/3'}>
-                <Map places={places} />
+            <Button className={`flex md:hidden w-full h-fit text-3xl py-4 rounded-none bg-snuxplore-black hover:bg-snuxplore-black absolute z-40 bottom-0 font-nohemi-medium text-snuxplore-yellow`} onClick={()=>setBottomDrawerStatus(true)}>
+                <ChevronUp size={38} className={'mr-3'}/> Navigate Campus
+            </Button>
+
+            <Drawer modal={true} open={bottomDrawerStatus} onClose={() => setBottomDrawerStatus(false)}>
+                <DrawerContent className={'z-50 bg-snuxplore-black border-snuxplore-black h-[85%]'}>
+                    <div className={'px-4 py-4 h-full flex flex-col'}>
+                        <div className={'text-2xl w-full text-center font-nohemi-semibold text-snuxplore-yellow'}>
+                            Navigate Campus
+                        </div>
+                        <div className={`text-sm text-white w-full text-justify mt-4`}>
+                            {"We get it, navigating the university can be challenging! But have no worries, We can connect you to resources that will unlock all that Shiv Nadar University has to offer."}
+                        </div>
+                        <Separator className={'w-full mt-4 bg-snuxplore-yellow'} />
+                        <div className={`mt-8 w-full transition-transform duration-300 font-nohemi-regular ${showHeader ? 'translate-y-0' : '-translate-y-full'}`}>
+                            <Input
+                                className={'w-full'}
+                                placeholder={"Search for location, admin info or anything else"}
+                                value={searchQuery}
+                                onChange={(e) => setSearchQuery(e.target.value)}
+                            />
+                        </div>
+                        <div className={'flex-1 mt-8 overflow-y-scroll space-y-4'}>
+                            {filteredPlaces.map((place) => (
+                                <NavigationCard key={place?.name} place={place as Places} />
+                            ))}
+                        </div>
+                    </div>
+                </DrawerContent>
+            </Drawer>
+
+            <div className={'h-full w-full md:w-2/3 z-30'}>
+                <Map places={filteredPlaces as Places[]}/>
             </div>
         </div>
     );
