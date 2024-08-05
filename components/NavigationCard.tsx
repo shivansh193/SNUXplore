3fb83ac2695d8d0458f6af689267d7702696d9f2
Rachit Kumar <rachit.prodigy205@gmail.com>
2024-08-05 20:09:11 +0530

    feat: add accent fonts to nav page

--- components/NavigationCard.tsx
+++ components/NavigationCard.tsx
@@ -11,12 +11,12 @@ export default function NavigationCard({ place }: NavigationCardProps) {
         <div className="w-full h-40 bg-white rounded-xl border-4 border-snuxplore-yellow flex flex-row items-center justify-start p-2 space-x-2">
             <img src={place.image} className="min-w-[50%] h-[100%] rounded" />
             <div className="w-full h-full flex flex-col items-start justify-start mt-2 relative">
-                <div className="text-snuxplore-black text-xl font-bold">{place.name}</div>
+                <div className="text-snuxplore-black text-xl font-bold font-nohemi-medium">{place.name}</div>
                 <div className="text-snuxplore-gray text-sm">
                     {place.description}
                 </div>
                 <a href={place.link}>
-                    <Button className="h-fit w-full bg-snuxplore-black text-md absolute bottom-2 py-1 px-2">Locate</Button>
+                    <Button className="h-fit w-full bg-snuxplore-black text-md absolute bottom-2 py-1 px-2  font-nohemi-regular">Locate</Button>
                 </a>
             </div>
         </div>
--- components/ScrollContainer.tsx
+++ components/ScrollContainer.tsx
@@ -35,14 +35,14 @@ const ScrollContainer: React.FC<ScrollContainerProps> = ({ places }) => {
     return (
         <div className={'h-screen w-full bg-white flex flex-row'}>
             <div className={'h-full w-1/3 bg-snuxplore-brown flex flex-col px-8 pt-8 justify-start items-center'}>
-                <div className={`w-full h-fit text-3xl text-white flex flex-row items-center transition-transform duration-300 ${showHeader ? 'translate-y-0' : '-translate-y-full'}`}>
+                <div className={`w-full h-fit text-3xl text-white flex flex-row items-center transition-transform duration-300 font-nohemi-semibold ${showHeader ? 'translate-y-0' : '-translate-y-full'}`}>
                     <img src={"/s.svg"} className={'mr-2'} />SNUXplore
                 </div>
-                <div className={`w-full h-fit text-3xl font-semibold mt-8 text-snuxplore-yellow transition-transform duration-300 ${showHeader ? 'translate-y-0' : '-translate-y-full'}`}>Navigate Campus</div>
+                <div className={`w-full h-fit text-3xl font-semibold mt-8 text-snuxplore-yellow transition-transform duration-300 font-nohemi-semibold ${showHeader ? 'translate-y-0' : '-translate-y-full'}`}>Navigate Campus</div>
                 <div className={`w-full h-fit text-md mt-2 text-white font-nohemi transition-transform duration-300 ${showHeader ? 'translate-y-0' : '-translate-y-full'}`}>
                     {"We get it, navigating the university can be challenging! But have no worries, We can connect you to resources that will unlock all that Shiv Nadar University has to offer."}
                 </div>
-                <div className={`mt-8 w-full transition-transform duration-300 ${showHeader ? 'translate-y-0' : '-translate-y-full'}`}>
+                <div className={`mt-8 w-full transition-transform duration-300 font-nohemi-regular ${showHeader ? 'translate-y-0' : '-translate-y-full'}`}>
                     <Input className={'w-full'} placeholder={"Search for location, admin info or anything else"} />
                 </div>
                 <div className={'w-full mt-8 h-full overflow-y-scroll space-y-4 no-scrollbar'}>
